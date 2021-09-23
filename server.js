2dfeeac184fa5f177903b4cb44ee1b230c7ed97d
flokamp <74511633+flokamp@users.noreply.github.com>
2021-09-22 20:25:59 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("Now listening"));
+});
